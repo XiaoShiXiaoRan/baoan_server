591be5240e67438695e7b45a5747ba143c23d69a
XiaoShiXiaoRan <1749512834@qq.com>
2023-03-10 16:18:40 +0800

    Initial commit

---
+++ src/main/java/com/springboot/baoan_server/BaoanServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.baoan_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoanServerApplication.class, args);
+    }
+
+}
